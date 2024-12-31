d7029d9c8da7ad2af6ef14bce9e9d1817ad40588
Luka Pantar <luka.pantar@gmail.com>
2024-12-31 12:22:28 +0100

    Removed database migrations.

--- scripts/migrate-and-run-in-docker.sh
+++ scripts/migrate-and-run-in-docker.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-cd /app/backend
+#mkdir /app/backend
+#cd /app/backend
 
-echo "Migrating database..."
-/root/.local/bin/poetry run python manage.py migrate
+#echo "Migrating database..."
+#/root/.local/bin/poetry run python manage.py migrate
 
 echo "Running server..."
 /root/.local/bin/poetry run python manage.py runserver --skip-checks --noreload 0.0.0.0:8000
